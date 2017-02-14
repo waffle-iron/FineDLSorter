d8218844c2cbc5eb2337ccf9119ed526b0b264cf
BrandonCravener <BrandonCravener@users.noreply.github.com>
2017-02-14 11:00:42 -0500

    2/3 Code Commented

--- index.js
+++ index.js
@@ -12,30 +12,47 @@ const mv = require('mv');
 
 // Setup function for checking debug mode before logging debug
 function DebugLog(message) {
+  // Check if the app is running in debug mode and if their is a message supplied
   if (config.debug && message) {
+    // A message has been supplied and the app is in debug mode
+    // Log the message given into the console
     console.log(message);
   }
 }
 
 // Check the given directory for folders if they dont exist create them
 fs.readdirSync(config.directory, (err, files) => {
+  // Check if their is an error provided
   if (err) {
+    // An error message has been supplied
+    // Log the error if in debug mode
     DebugLog(err);
   } else {
+    // An error message hasen't been supplied
+    // Itterate through the sorting categorys in the config
     for (let i = 0; i < Object.keys(config.sorting).length; i += 1) {
+      // Define a variabel to hold the category name
       const folder = Object.keys(config.sorting)[i];
+      // Check if the folder has a subdirectory for the category
       if (!files.includes(folder)) {
+        // The directory doesn't contain a sub directory for the category
+        // Create a new folder in the supplied download directory
         fs.mkdir(`${config.directory}/${folder}`, (error) => {
+          // Attempt to log error to console if their is one
           DebugLog(error);
         });
       }
+      // Check for the other folder in the download directory provided
       if (!files.includes(config.others_name)) {
+        // Create a new directory in the supplied download directory
         fs.mkdir(`${config.directory}/${config.others_name}`, (error) => {
+          // Attempt to log error to console if their is one
           DebugLog(error);
         });
       }
     }
   }
+  // Log a message that all of the folders have been processed
   DebugLog('Folders have been processed');
 });
 
