17e1b5ac2ffa9ae91b0b03091994bc3da6609f14
BrandonCravener <BrandonCravener@users.noreply.github.com>
2017-02-14 14:20:27 -0500

    Fully Commented

--- index.js
+++ index.js
@@ -58,42 +58,66 @@ fs.readdirSync(config.directory, (err, files) => {
 
 // Create an array to contain the file path of ignored files
 const ignoredFiles = [];
+// Itterate through the list of ignored files
 for (let i = 0; i < config.ignore.length; i += 1) {
+  // Push the file to the array
   ignoredFiles.push(`${config.directory}/${config.ignore[i]}`);
 }
 
 // Define the download folder watcher to detect file system changes
 const watcher = chokidar.watch(config.directory, {
-  ignored: ignoredFiles,
-  persistent: true,
-  depth: 0,
+  ignored: ignoredFiles, // Ignore the files supplied
+  persistent: true, // Keep the process running while the files are being watched
+  depth: 0, // Dont watch subdirectories of the the download folder 
 });
 
+// Wait for new files to be added to the download folder
 watcher.on('add', (path) => {
+  // If debug then log the file that has been added
   DebugLog(`Directory ${path} has been added`);
+  // Get the file extension of the file added
   const fileExt = pth.extname(path);
+  // If debug log the file extension
   DebugLog(fileExt);
+  // Define a variable to keep track of the sorting state
   let sorted = false;
+  // Itterate through all of the sorting categorys
   for (let i = 0; i < Object.keys(config.sorting).length; i += 1) {
+    // Create a variable to contain the category name
     const folder = Object.keys(config.sorting)[i];
+    // If debug log the category name to the console
     DebugLog(folder);
+    // Check if the category holds the extension of the new file
     if (config.sorting[folder].includes(fileExt)) {
+      // The category does contain the file extension
+      // Create a timer to delay moving the file
       setTimeout(() => {
+        // If debug log where the file will be moved from and where to
         DebugLog(`Moving ${path} to ${pth.dirname(path)}/${folder}/${pth.basename(path)}`);
+        // Move the file to the folder for the category matched
         mv(path, `${pth.dirname(path)}/${folder}/${pth.basename(path)}`, (err) => {
+          // If their is and error and it is in debug mode log the error
           DebugLog(err);
         });
-      }, config.delay * 1000);
+      }, config.delay * 1000); // Calculate the delay of the file move and set it
+      // Update the sorting status variable so the app knows the files sorted
       sorted = true;
+      // Stop the for loop early
       break;
     }
   }
+  // Test if the file is not sorted
   if (!sorted) {
+    // The file is not sorted
+    // Create a timer to delay moving the file
     setTimeout(() => {
+      // Log that the file will be moved to the other folder if its in debug mode
       DebugLog(`Moving ${path} to ${pth.dirname(path)}/${config.others_name}/${pth.basename(path)}`);
+      // Move the file to the other folder
       mv(path, `${pth.dirname(path)}/${config.others_name}/${pth.basename(path)}`, (err) => {
+        // Log the error if their is one in debug mode
         DebugLog(err);
       });
-    }, config.delay * 1000);
+    }, config.delay * 1000); // Calculate the delay of the file move and set it
   }
 });
